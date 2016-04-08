3072f8ebdfed059ce64519abfc2b1e07f797a1c9
Adam Schønemann <adamschoenemann@gmail.com>
2016-04-08 15:51:01 +0200

    fix indentation in DB.java

--- src/dk/itu/ssas/project/DB.java
+++ src/dk/itu/ssas/project/DB.java
@@ -8,13 +8,13 @@ public class DB {
 	public static Connection getConnection() throws SQLException {
 		// Better way to do this is setting database connect info in
 		// servlet context; but for the SSAS project, that just adds
-		// complications. 
+		// complications.
 		MysqlDataSource ds = null;
-		
-			ds = new MysqlDataSource();
-			ds.setUrl("jdbc:mysql://localhost:3306/image_site_db");
-			ds.setUser("SSAS");
-			ds.setPassword("SSAS");
-			return ds.getConnection();
+
+		ds = new MysqlDataSource();
+		ds.setUrl("jdbc:mysql://localhost:3306/image_site_db");
+		ds.setUser("SSAS");
+		ds.setPassword("SSAS");
+		return ds.getConnection();
 	}
 }
